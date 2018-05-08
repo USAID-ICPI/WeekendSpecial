cb79ddfdb8b10ca5391e53069cab7941acadbe7f
Aaron <achafetz@usaid.gov>
2018-05-08 13:58:52 -0400

    add export feature

--- R/wpm_combine.R
+++ R/wpm_combine.R
@@ -10,7 +10,7 @@
 #'  \dontrun{
 #'  df_weekly <- wpm_combine("~/Week_23")}
 
-wpm_combine <- function(folderpath_reports, folderpath_output = NULL){ 
+wpm_combine <- function(folderpath_reports, folderpath_sitecoords = NULL, folderpath_output = NULL){ 
 
   #genreate full list of sheets within each file
     df_full_list <- wpm_identify(folderpath_reports)
@@ -18,6 +18,9 @@ wpm_combine <- function(folderpath_reports, folderpath_output = NULL){
   #combine all data into one df
     df_full_weekly <- purrr::map2_dfr(df_full_list$path, df_full_list$sheet_name,
                                    ~ wpm_import(.x, .y))
+  #add coordinates
+    if(!is.null(folderpath_output)){
+      df_full_weekly <- wpm_map(df_full_weekly, folderpath_sitecoords)
   #export
     if(!is.null(folderpath_output)){
     readr::write_csv(df_full_weekly, 
