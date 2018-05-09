6256215d5de7291a4482f65f53472cb2f81b0894
Aaron <achafetz@usaid.gov>
2018-05-09 16:10:36 -0400

    move select/arrange from combine to map

--- R/wpm_map.R
+++ R/wpm_map.R
@@ -11,19 +11,26 @@
 #'  df <- wpm_combine(df,"~/WPM/GIS")}
 
 wpm_map <- function(df, folderpath_orgunits){
+  
   if(!is.null(folderpath_orgunits)){
-  #import coordinates file
+    #import coordinates file
     coord <- readr::read_csv(Sys.glob(file.path(folderpath_orgunits, "*sites*.csv")))
-  #merge onto main df
+    #merge onto main df
     df <- dplyr::left_join(df, coord, by = c("facility" = "name"))
   } else {
-    #add blank columsn if the coordinates file does not exist
+    #add blank columns if the coordinates file does not exist
     df <- df %>% 
       mutate(facility_uid = NA, 
              district_uid = NA, 
              lat = NA, 
              long = NA)
   }
+  
+  #arrange
+  df <- df %>% 
+    select(partner, district, district_uid, sub_district, facility, 
+           facility_uid, lat, long, provincial_lead, site_lead, indicator, 
+           date, quarter, value)
   
   return(df)
   
