8baf636cd34a0ff8c0adfd48c3207df181ce2f51
Aaron <achafetz@usaid.gov>
2018-05-09 17:29:18 -0400

    easy run

--- OtherScripts/package_dev.R
+++ OtherScripts/package_dev.R
@@ -16,5 +16,8 @@ check()
 
 #Call new GetSites function to get site locations as df; must reference OU of interest by UID and site level by #
 datimvalidation::loadSecrets(secrets = NA)
-df <- wpm_getcoords("cDGPF739ZZr", 7, "GIS")
+wpm_getcoords("cDGPF739ZZr", 7, "GIS")
+
+df <- wpm_combine("RawData", "GIS", "Output")
+
 
