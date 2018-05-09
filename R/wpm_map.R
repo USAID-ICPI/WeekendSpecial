a214cc35a474c9e3a0acf0ce6929eb0aa55a3ac8
Aaron <achafetz@usaid.gov>
2018-05-09 15:59:56 -0400

    add filler columns if coords file is missing

--- R/wpm_map.R
+++ R/wpm_map.R
@@ -11,9 +11,20 @@
 #'  df <- wpm_combine(df,"~/WPM/GIS")}
 
 wpm_map <- function(df, folderpath_orgunits){
+  if(!is.null(folderpath_orgunits)){
   #import coordinates file
     coord <- readr::read_csv(Sys.glob(file.path(folderpath_orgunits, "*sites*.csv")))
   #merge onto main df
-    df2 <- dplyr::left_join(df, coord, by = c("facility" = "name"))
+    df <- dplyr::left_join(df, coord, by = c("facility" = "name"))
+  } else {
+    #add blank columsn if the coordinates file does not exist
+    df <- df %>% 
+      mutate(facility_uid = NA, 
+             district_uid = NA, 
+             lat = NA, 
+             long = NA)
+  }
+  
+  return(df)
   
 }
