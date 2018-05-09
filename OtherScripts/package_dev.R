6bfeb14b82933225ae53a91496ee1b79f4b294ee
Aaron <achafetz@usaid.gov>
2018-05-09 16:00:42 -0400

    minor adj

--- OtherScripts/package_dev.R
+++ OtherScripts/package_dev.R
@@ -14,5 +14,7 @@ library("WeekendSpecial")
 
 check()
 
-
+#Call new GetSites function to get site locations as df; must reference OU of interest by UID and site level by #
+datimvalidation::loadSecrets(secrets = NA)
+df <- wpm_getcoords("cDGPF739ZZr", 7, "GIS")
 
