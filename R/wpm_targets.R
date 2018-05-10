44c7951a34ade8554e84f7cf247b4fa19b34546e
Aaron <achafetz@usaid.gov>
2018-05-10 17:53:03 -0400

    comment out process

--- R/wpm_targets.R
+++ R/wpm_targets.R
@@ -11,29 +11,36 @@
 #'   wpm_targets("~/data/Burundi_msd.txt", "~/project", "Burundi")}
 wpm_targets <- function(filepath_msd, folder_output, countryname) {
 
-df <- readr::read_tsv(filepath_msd,  
-                      col_type  = cols_only(Facility = "c",
-                                           FY2018_TARGETS = "d",
-                                           FacilityUID = "c",
-                                           indicator = "c",
-                                           standardizedDisaggregate = "c",
-                                           SNU1 = "c",
-                                           PSNU = "c",
-                                           Community = "c",
-                                           CommunityUID = "c"))%>% 
-                      dplyr::rename_all(~ tolower(.))
-
-df <- df %>% 
-  dplyr::filter((indicator %in% c("HTS_TST", "HTS_TST_POS", "TX_NEW", "TX_CURR")),
-    standardizeddisaggregate=="Total Numerator", !is.na(fy2018_targets), !is.na(facilityuid)) %>%
-  dplyr::select(-standardizeddisaggregate) %>% 
-  dplyr::group_by_if(is.character) %>% 
-  dplyr::summarise(fy2018_targets = sum(fy2018_targets)) %>% 
-  dplyr::ungroup()
-
-readr::write_csv(df, file.path(folderpath_output, paste0(countryname, "_fy18_site_targets.csv")), na="")
+  #import site MER Structured Dataset, specifying cols to keep
+    df <- readr::read_tsv(filepath_msd,  
+                          col_type  = cols_only(Facility = "c",
+                                                FY2018_TARGETS = "d",
+                                                FacilityUID = "c",
+                                                indicator = "c",
+                                                standardizedDisaggregate = "c",
+                                                SNU1 = "c",
+                                                PSNU = "c",
+                                                Community = "c",
+                                                CommunityUID = "c"))%>% 
+          dplyr::rename_all(~ tolower(.))
   
-}
+  #munge
+    df <- df %>% 
+      #filter to just key variables tracked weekly & drop targets not tied to facilities
+      dplyr::filter(indicator %in% c("HTS_TST", "HTS_TST_POS", "TX_NEW", "TX_CURR"),
+                    standardizeddisaggregate=="Total Numerator", 
+                    !is.na(fy2018_targets), 
+                    !is.na(facilityuid)) %>%
+      #remove disagg since just needed to pull out total num
+      dplyr::select(-standardizeddisaggregate) %>%
+      #summarize to catch any targets on multiple rows
+      dplyr::group_by_if(is.character) %>% 
+      dplyr::summarise(fy2018_targets = sum(fy2018_targets)) %>% 
+      dplyr::ungroup()
+  
+  readr::write_csv(df, file.path(folderpath_output, paste0(countryname, "_fy18_site_targets.csv")), na="")
+    
+  }
   
   
   
