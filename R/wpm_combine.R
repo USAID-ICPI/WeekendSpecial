3405e3b4da2ee2f66158e27d95302651238ff00f
Aaron <achafetz@usaid.gov>
2018-05-11 11:49:56 -0400

    update with targets and mech ids

--- R/wpm_combine.R
+++ R/wpm_combine.R
@@ -2,6 +2,7 @@
 #'
 #' @param folderpath_reports what folder are all the partner reports stored in?
 #' @param folderpath_sitecoords what folder is the site coordinates files stored in? If no folder is noted, no coordinates will be merged on
+#' @param folderpath_targets what folder is the targets file stored in?
 #' @param folderpath_output what folder would you like to save the combines output dataset in? If no folder is noted, no file will be saved
 #'
 #' @export
@@ -11,7 +12,7 @@
 #'  \dontrun{
 #'  df_weekly <- wpm_combine("~/Week_23")}
 
-wpm_combine <- function(folderpath_reports, folderpath_sitecoords = NULL, folderpath_output = NULL){ 
+wpm_combine <- function(folderpath_reports, folderpath_sitecoords = NULL, folderpath_targets = NULL, folderpath_output = NULL){ 
 
   #genreate full list of sheets within each file
     df_full_list <- wpm_identify(folderpath_reports)
@@ -19,9 +20,14 @@ wpm_combine <- function(folderpath_reports, folderpath_sitecoords = NULL, folder
   #combine all data into one df
     df_full_weekly <- purrr::map2_dfr(df_full_list$path, df_full_list$sheet_name,
                                    ~ wpm_import(.x, .y))
+  #add mechanism id
+    df_full_weekly <- wpm_addmechid(df_weekly_full)
+      
   #add coordinates
     df_full_weekly <- wpm_map(df_full_weekly, folderpath_sitecoords)
-
+  
+  #add weekly targets
+    df_full_weekly <- wpm_addtargets(df_full_weekly, folderpath_targets)
   #export
     if(!is.null(folderpath_output)){
     readr::write_csv(df_full_weekly, 
