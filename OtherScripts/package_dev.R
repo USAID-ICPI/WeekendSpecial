96f92e514e4525ee0245a3ab5379e6977894116b
Aaron Chafetz <achafetz@usaid.gov>
2018-05-21 12:37:20 -0400

    adjust for testing

--- OtherScripts/package_dev.R
+++ OtherScripts/package_dev.R
@@ -13,10 +13,18 @@ build()
 
 
 #test
+  folderpath_reports <- "RawData"
+  folderpath_sitecoords <- "GIS"
+  folderpath_targets <-  "Output"
+  folderpath_output <- "Output"
+  
   library(magrittr)
   
   wpm_filerefresh("C:/Users/achafetz/Downloads/drive-download-20180510T140330Z-001.zip",
                   "C:/Users/achafetz/Documents/GitHub/WeekendSpecial/RawData")
+  
+  filepath <- df_full_list$path[1]
+  sheet_name <- df_full_list$sheet_name[1]
   
   list <- wpm_identify("RawData")
    
