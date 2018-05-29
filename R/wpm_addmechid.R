2c05568753248bb35ccc6464c6b3f321f036f119
Aaron <achafetz@usaid.gov>
2018-05-29 14:22:07 -0400

    adjust partner names

--- R/wpm_addmechid.R
+++ R/wpm_addmechid.R
@@ -14,11 +14,11 @@ wpm_addmechid <- function(df){
   dplyr::mutate(mechanismid = dplyr::case_when(
     partner == "ANOVA"                  ~ "11111",
     partner == "BRHC"                   ~ "11111",
-    partner == "FDP"                    ~ "11111",
+    partner == "FPD"                    ~ "11111",
     partner == "KI"                     ~ "11111",
     partner == "MatCH"                  ~ "11111",
     partner == "RTC"                    ~ "11111",
-    partner == "WRHI"                   ~ "11111",
+    partner == "Wits RHI"               ~ "11111",
     partner == "Aurum Health Research"  ~ "18484",
     partner == "Health Systems Trust"   ~ "18481",
     partner == "TB/HIV Care"            ~ "18482")
