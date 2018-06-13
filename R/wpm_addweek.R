f1f007a66bd30558c8b76cf6bafcfde83366b774
Aaron Chafetz <achafetz@usaid.gov>
2018-06-13 11:15:24 -0400

    add in month column per request

--- R/wpm_addweek.R
+++ R/wpm_addweek.R
@@ -1,5 +1,5 @@
 
-#' Add week number
+#' Add FY week number & month
 #'
 #' @param df data frame to add week number to
 #' @param date_var unquoted variable that contains the date
@@ -26,11 +26,12 @@ wpm_addweek <- function(df, date_var, fy_start = "2017-10-02"){
     
   #add blank column for ordered placeholder
     df <- df %>% 
-      tibble::add_column(fy_week = NA, .after = loc)
+      tibble::add_column(month = NA, fy_week = NA, .after = loc)
     
   #add date
     df <- df %>% 
-      dplyr::mutate(fy_week = as.integer((!!date_var - fy_start)/7 + 1))
+      dplyr::mutate(month = lubridate::month(!!date_var, label = TRUE),
+                    fy_week = as.integer((!!date_var - fy_start)/7 + 1))
     
   return(df)
   
