5b7ccb1bb6a2b8772b189cf07eb8ee053d6eb7f7
Aaron Chafetz <achafetz@usaid.gov>
2018-08-29 17:06:54 -0400

    adjust for pulling in all hierarchy from DATIM site file

--- R/wpm_map.R
+++ R/wpm_map.R
@@ -17,15 +17,23 @@ wpm_map <- function(df, folderpath_orgunits){
     coord <- readr::read_csv(Sys.glob(file.path(folderpath_orgunits, "*sites*.csv")),
                              col_types = readr::cols(.default = "c")) %>% 
       dplyr::mutate_at(dplyr::vars(latitude, longitude), ~ as.numeric(.))
-    
+    #drop hiearchy contained in orginal files
+    df <- dplyr::select(df, -c(province, district))
     #merge onto main df
     df <- dplyr::left_join(df, coord, by = "facility")
   } else {
     #add blank columns if the coordinates file does not exist
     df <- df %>% 
-      dplyr::mutate(facilityuid = NA, 
-             latitude = NA, 
-             longitude = NA)
+      dplyr::mutate(operatingunit = NA,
+                    snu1          = NA, 
+                    snu1uid       = NA,
+                    psnu          = NA,
+                    psnuuid       = NA,
+                    facilityuid   = NA,
+                    community     = NA,
+                    facilityuid   = NA,
+                    latitude      = NA, 
+                    longitude     = NA)
   }
 
   return(df)
