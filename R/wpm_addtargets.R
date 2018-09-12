b96f2be200b2a4c3add6c7559773e8921e3bf2ab
Aaron Chafetz <achafetz@usaid.gov>
2018-09-12 11:43:33 -0400

    filter targets to relevant inds/mechs; duplicate targets for each week

--- R/wpm_addtargets.R
+++ R/wpm_addtargets.R
@@ -26,7 +26,21 @@ wpm_addtargets <- function(df, folderpath_targets){
       
     #clean up target file prior to merge (make sure we can merge (may be missing hierarchy if don't have site file))
       targets <- dplyr::select(targets, -c(snu1,  snu1uid, psnuuid, facility))
-  
+    
+    #limit target file to what is in the df
+      lst_facilities <- unique(df$facilityuid)
+      lst_mechs <- unique(df$mechanismid)
+      targets <- targets %>% 
+        dplyr::filter(facilityuid %in% lst_facilities, mechanismid %in% lst_mechs)
+    
+    #duplicate each target for each week
+      n_weeks <- max(df$fy_week)
+      targets_wk <-  
+        purrr::map_df(seq_len(n_weeks), ~ targets) %>% 
+        dplyr::group_by(mechanismid, facilityuid, indicator, target_wkly) %>% 
+        dplyr::mutate(id = row_number())  %>% 
+        dplyr::ungroup()
+      
     #merge onto main df
       df <- dplyr::left_join(df, targets, by = c("facilityuid", "mechanismid", "indicator"))
     
